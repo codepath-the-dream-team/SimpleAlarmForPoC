4156f8d758f8eb62f933269616d0841bf36b4353
Chihiro Saito <chihiro@Chihiros-MacBook-Pro.local>
2016-11-12 16:45:49 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+A very simple AudioPlayer that starts playing a designated mp3 file after a certain delay.  
--- SimpleAlarm/ViewController.swift
+++ SimpleAlarm/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var alarmButton: UIButton!
+    
+    let mp3path = "https://dream-team-bucket.s3-us-west-1.amazonaws.com/music/rainforest.mp3"
+    var player = AVPlayer()
+    var alarmScheduled = false
+    let alarmDuration = 10 // 10 seconds
+    var countdown = 0 {
+        didSet {
+            self.setTimerCountdownLabel(countdown)
+        }
+    }
+    var timer : Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        alarmButton.setTitle("Stop Alarm", for: UIControlState.selected)
+        self.initPlayer(self.mp3path)
+        self.countdown = self.alarmDuration
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func onButtonClick(_ sender: UIButton) {
+        if (!alarmScheduled) {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerFired), userInfo: nil, repeats: true)
+        } else {
+            if (countdown > 0) {
+                self.timer!.invalidate()
+            } else {
+                self.player.pause()
+                self.countdown = self.alarmDuration
+            }
+        }
+        alarmScheduled = !alarmScheduled
+        alarmButton.isSelected = alarmScheduled
+    }
+    
+    func initPlayer(_ path: String) {
+        let url = URL(string: path)
+        let playerItem = AVPlayerItem(url: url!)
+        self.player = AVPlayer(playerItem: playerItem)
+        self.player.volume = 1.0
+    }
+    
+    func timerFired() {
+        countdown -= 1
+        if (countdown == 0) {
+            self.timer!.invalidate()
+            self.player.play()
+        }
+    }
+    
+    func setTimerCountdownLabel(_ seconds: Int) {
+        let (h,m,s) = secondsToHoursMinutesSeconds(seconds:seconds)
+        timerLabel.text = String(format: "%02d:%02d:%02d", h,m,s)
+    }
+    
+    
+    func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
+        return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    }
 
 }
 
